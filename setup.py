243bd95c2b6b655fdc886bf5f5fa661fa99e035c
Michael Chow <machow@princeton.edu>
2017-07-27 14:11:19 -0400

    some scaffolding

---
+++ setup.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+import re
+import ast
+from setuptools import setup
+
+_version_re = re.compile(r'__version__\s+=\s+(.*)')
+
+with open('shellwhat/__init__.py', 'rb') as f:
+    version = str(ast.literal_eval(_version_re.search(
+        f.read().decode('utf-8')).group(1)))
+
+setup(
+	name='shellwhat',
+	version=version,
+	packages=['shellwhat'],
+	install_requires=['markdown2'],
+        description = 'Submission correctness tests for shell languages',
+        author = 'Michael Chow',
+        author_email = 'michael@datacamp.com',
+        url = 'https://github.com/datacamp/shellwhat')
