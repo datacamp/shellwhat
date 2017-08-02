0f6a418a382c7120ebaedacab362b5c24dfa46e2
Michael Chow <machow@princeton.edu>
2017-08-02 12:16:49 -0400

    remove some commented out lines

--- shellwhat/State.py
+++ shellwhat/State.py
@@ -5,8 +5,5 @@ class State(BaseState):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
-        # just in case the code is valid postgresql, set as if couldn't parse.
-        # self.solution_ast = self.student_ast = self.ast_dispatcher.ast.AntlrException('', '')
-
     def get_dispatcher(self):
         return None
