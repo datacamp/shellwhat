b39b483e2aa4e201f7940279cd468fa14748b017
Michael Chow <machow@princeton.edu>
2017-08-22 13:45:53 -0400

    osh parser to AST running locally

--- shellwhat/State.py
+++ shellwhat/State.py
@@ -1,4 +1,20 @@
+from protowhat.selectors import Dispatcher
 from protowhat.State import State as BaseState
+
+from protowhat.utils_ast import AstModule
+
+
+from subprocess import check_output
+import shlex
+import json
+
+#PARSER_OSH_STUB = ["python2", "-m", "osh"]
+PARSER_OSH_STUB = ["docker", "exec", "oilc", "python2", "-m", "osh"]
+
+def parse_osh(cmd):
+    sanitized_cmd = shlex.quote(cmd)
+    res = check_output(PARSER_OSH_STUB + [sanitized_cmd])
+    return json.loads(res.decode())
 
 class State(BaseState):
 
@@ -6,4 +22,4 @@ class State(BaseState):
         super().__init__(*args, **kwargs)
 
     def get_dispatcher(self):
-        return None
+        return AstModule.from_parse_dict(parse_osh)
