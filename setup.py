e5d0d8ffd02d24d2a52763822823427a3443176e
Jeroen Hermans <jeroen@datacamp.com>
2018-12-14 14:56:20 +0100

    Improve setup

--- setup.py
+++ setup.py
@@ -5,28 +5,43 @@ import ast
 from os import path
 from setuptools import setup
 
-_version_re = re.compile(r'__version__\s+=\s+(.*)')
+PACKAGE_NAME = "shellwhat"
+REQUIREMENT_NAMES = ["protowhat"]
 
-PACKAGE_NAME = 'shellwhat'
 HERE = path.abspath(path.dirname(__file__))
-with open(path.join(HERE, 'README.md'), encoding='utf-8') as fp:
+VERSION_FILE = path.join(HERE, PACKAGE_NAME, "__init__.py")
+REQUIREMENTS_FILE = path.join(HERE, "requirements.txt")
+README_FILE = path.join(HERE, "README.md")
+
+with open(VERSION_FILE, encoding="utf-8") as fp:
+    _version_re = re.compile(r"__version__\s+=\s+(.*)")
+    VERSION = str(ast.literal_eval(_version_re.search(fp.read()).group(1)))
+with open(REQUIREMENTS_FILE, encoding="utf-8") as fp:
+    req_txt = fp.read()
+    _requirements_re_template = r"^({}(?:\s*[<>=]+\s*\S*)?)\s*(?:#.*)?$"
+    REQUIREMENTS = [
+        re.search(_requirements_re_template.format(requirement), req_txt, re.M).group(0)
+        for requirement in REQUIREMENT_NAMES
+    ]
+with open(README_FILE, encoding="utf-8") as fp:
     README = fp.read()
-with open(path.join(HERE, PACKAGE_NAME, '__init__.py'), 'rb') as fp:
-    VERSION = str(ast.literal_eval(_version_re.search(
-        fp.read().decode('utf-8')).group(1)))
 
 setup(
-	name=PACKAGE_NAME,
-	version=VERSION,
-	packages=['shellwhat', 'shellwhat.checks'],
-	install_requires=['protowhat>=0.5.0'],
-    description = 'Submission correctness tests for shell languages',
+    name=PACKAGE_NAME,
+    version=VERSION,
+    packages=[PACKAGE_NAME, "{}.checks".format(PACKAGE_NAME)],
+    install_requires=REQUIREMENTS,
+    description="Submission correctness tests for shell languages",
     long_description=README,
-    long_description_content_type='text/markdown',
-    license='GNU version 3',
-    author='Michael Chow',
-    author_email='michael@datacamp.com',
-    maintainer='Filip Schouwenaars',
-    maintainer_email='filip@datacamp.com',
-    url = 'https://github.com/datacamp/shellwhat'
+    long_description_content_type="text/markdown",
+    author="Michael Chow, Filip Schouwenaars",
+    author_email="michael@datacamp.com",
+    maintainer="Jeroen Hermans",
+    maintainer_email="content-engineering@datacamp.com",
+    url="https://github.com/datacamp/shellwhat",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU Affero General Public License v3",
+        "Operating System :: OS Independent",
+    ],
 )
